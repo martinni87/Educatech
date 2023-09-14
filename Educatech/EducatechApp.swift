db02aea22e3599b51cdca450062f312c3ab1b505
Martin Antonio <30123957+martinni87@users.noreply.github.com>
2023-09-14 18:10:39 +0200

    Initial Commit

---
+++ Educatech/EducatechApp.swift
@@ -0,0 +1,17 @@
+//
+//  EducatechApp.swift
+//  Educatech
+//
+//  Created by Martín Antonio Córdoba Getar on 14/9/23.
+//
+
+import SwiftUI
+
+@main
+struct EducatechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
